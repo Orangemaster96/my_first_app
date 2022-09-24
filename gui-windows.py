7ce8c2a5de6b63e10fdb9e2e1aa6c8a8bb1a8465
Kevin Pastrana <107160973+Orangemaster96@users.noreply.github.com>
2022-09-24 13:07:20 -0500

    Add files via upload

--- gui-windows.py
+++ gui-windows.py
@@ -3,28 +3,27 @@ import PyPDF2
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 
-
-
 root = tk.Tk()
 
 canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3, rowspan=5)
 
-
 #logo
-logo = Image.open('C:\myfirstapp\logo.png')
+logo = Image.open('\\Users\Kevin-admin\OneDrive\Documents\My_code\Python\myfirstapp\\blur.png')
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 logo_label.image = logo
 logo_label.grid(column=1, row=0)
 
+#word to find
+search_word = tk.Entry()
+search_word.grid(column=2, row=4)
+
 #page sel entry
 page_sel = tk.Entry()
 page_sel.grid(column=1, row=4)
 
-
 #instructions
-
 instructions = tk.Label(root, text = 'Select pdf file', font = 'Calibri')
 instructions.grid(columnspan = 3, column=0, row=1)
 
@@ -34,12 +33,15 @@ def open_file():
     file= askopenfile(parent=root, mode='rb', title='choosefile', filetype=[('PDF File', '*.pdf')])
     
     if file:
-        #pagecount display
+        #pagecount display label
         read_pdf = PyPDF2.PdfFileReader(file)
         
         pagecount = int(read_pdf.getNumPages())
         pagecount_display = tk.Label(root, text = f'Pagecount is: {pagecount}, choose page to convert:', font = 'Calibri')
-        pagecount_display.grid(columnspan=3, column=0, row=3)
+        pagecount_display.grid(columnspan=2, column=0, row=3)
+        #word search label
+        search_word_label = tk.Label(root, text = 'Enter word to search (case sensitive):', font = 'Calibri')
+        search_word_label.grid(column=2, row=3)
         #convert button
         convert_text = tk.StringVar()
         convert_btn = tk.Button(root, textvariable=convert_text, font='Calibri', bg='#20bebe', fg='white', height=2, width=15, command=lambda:convert_file())
@@ -48,25 +50,32 @@ def open_file():
         #file conversion
         def convert_file():
             read_pdf = PyPDF2.PdfFileReader(file)
-            page = read_pdf.getPage(int(page_sel.get()))
+            page = read_pdf.getPage(int(page_sel.get())-1)
             page_content =page.extractText()
-            detect_word_t = "SEO"
-            #print(detect_word_t)
-            #detect_word = str(page_content.find('Seo'))
+            detect_word_t = search_word.get()
+            #Detect search word
             if detect_word_t:
-                def substring_after(s, delim):
-                    list_of_words = s.split()
-                    next_word= list_of_words[list_of_words.index(delim)+1]
-                    return next_word
-                    #return s.partition(delim)[2:3]
+                if str(detect_word_t) in page_content:
+                    #get string after
+                    def substring_after(s, delim):
+                        list_of_words = s.split()
+                        if list_of_words[list_of_words.index(delim)]:
+                        
+                            next_word= list_of_words[list_of_words.index(delim)+1]
+                            if next_word:
+                                return next_word
+                        else:
+                            print('Word not found')
+                    #insert then display word
+                    word = substring_after(page_content, str(detect_word_t))
+                    print(word)
+                    word_display =tk.Label(root, text = f'{word}', font = 'Calibri')
+                    word_display.grid(columnspan = 2, column=0, row=6)
+                else:
+                    word_display =tk.Label(root, text = f'{str(detect_word_t)} not found', font = 'Calibri')
+                    word_display.grid(columnspan = 2, column=0, row=6)
+                    print('did not work')
                 
-                word = substring_after(page_content, str(detect_word_t))
-                print(word)
-                word_display =tk.Label(root, text = f'{word}', font = 'Calibri')
-                word_display.grid(columnspan = 3, column=0, row=6)
-            else:
-                print('did not work')
-                #detected_word = page_content.find('seo')
             #textbox
             text_box = tk.Text(root, height=15, width=50, padx=15, pady=15)
             text_box.insert(1.0, page_content)
@@ -81,11 +90,8 @@ browse_btn = tk.Button(root, textvariable=browse_text, font='Calibri', bg='#20be
 browse_text.set('Browse')
 browse_btn.grid(column=1, row=2)
 
-
-
 canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3)
 
-
-
+#close loop
 root.mainloop()
